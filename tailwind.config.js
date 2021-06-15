a0d41d860c35a47a69696353b91737049543834b
Robby de Leon <84078050+obideleon@users.noreply.github.com>
2021-06-15 11:15:45 +0800

    added custom colors in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,33 @@ module.exports = {
         sans: ["Inter", ...defaultTheme.fontFamily.sans],
       },
     },
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      blue: {
+        dark: "#455AF7",
+        DEFAULT: "#2C71FF",
+        light: "#B7E6FF",
+        lightest: "#E5F6FF",
+      },
+      orange: {
+        DEFAULT: "#FF562D",
+        light: "#FFDAC9",
+        lightest: "#FFF0E9",
+      },
+      purple: {
+        DEFAULT: "#604BBA",
+        light: "#BCC3FF",
+      },
+      green: {
+        DEFAULT: "#348D50",
+      },
+      gray: {
+        DEFAULT: "#323232",
+        light: "#646464",
+        lightest: "#969696",
+      },
+    },
   },
   variants: {
     extend: {},
