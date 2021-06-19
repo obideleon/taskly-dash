32530dbf6a8e8b696d029c93f34e510f46337c74
Robby de Leon <84078050+obideleon@users.noreply.github.com>
2021-06-19 18:41:38 +0800

    modified Rank table

--- components/Rank.js
+++ components/Rank.js
@@ -2,6 +2,54 @@ import RankCard from "./RankCard";
 import React from "react";
 
 const Rank = () => {
+  const users = [
+    {
+      name: "Aaseif Rahman",
+      rank: "1",
+      task: "32",
+      hours: "32:40",
+      score: "+320",
+      totalScore: "9886",
+      profilePic: "user-1",
+    },
+    {
+      name: "Lexy Sanz",
+      rank: "2",
+      task: "28",
+      hours: "28:43",
+      score: "-228",
+      totalScore: "3234",
+      profilePic: "user-2",
+    },
+    {
+      name: "Krawl Brown",
+      rank: "3",
+      task: "26",
+      hours: "26:40",
+      score: "-246",
+      totalScore: "2836",
+      profilePic: "user-3",
+    },
+    {
+      name: "Brian Erjas",
+      rank: "4",
+      task: "24",
+      hours: "24:56",
+      score: "+320",
+      totalScore: "2468",
+      profilePic: "user-4",
+    },
+    {
+      name: "Mary Estudillo",
+      rank: "5",
+      task: "22",
+      hours: "22:44",
+      score: "-226",
+      totalScore: "2266",
+      profilePic: "user-5",
+    },
+  ];
+
   return (
     <div className="p-6 w-full">
       <div className="flex items-center justify-between">
@@ -15,7 +63,7 @@ const Rank = () => {
       <div className="mt-6">
         <table className="w-full">
           <thead>
-            <tr className="border-b  border-white-100 text-gray sm:text-sm text-xs text-left">
+            <tr className="border-b border-white-100 text-gray sm:text-sm text-xs text-left">
               <th className="pb-2 w-2/12 font-normal">Team Member Profile</th>
               <th className="text-center pb-2 w-1/12 font-normal">Rank</th>
               <th className="hidden sm:table-cell text-center pb-2 w-1/12 font-normal">
@@ -33,51 +81,9 @@ const Rank = () => {
             </tr>
           </thead>
           <tbody className="align-baseline">
-            <RankCard
-              name="Aaseif Rahman"
-              rank="1"
-              task="32"
-              hours="32:40"
-              score="+320"
-              totalScore="9886"
-              profilePic="user-1"
-            />
-            <RankCard
-              name="Lexy Sanz"
-              rank="2"
-              task="28"
-              hours="28:43"
-              score="-228"
-              totalScore="3234"
-              profilePic="user-2"
-            />
-            <RankCard
-              name="Krawl Brown"
-              rank="3"
-              task="26"
-              hours="26:40"
-              score="-246"
-              totalScore="2836"
-              profilePic="user-3"
-            />
-            <RankCard
-              name="Brian Erjas"
-              rank="4"
-              task="24"
-              hours="24:56"
-              score="+320"
-              totalScore="2468"
-              profilePic="user-4"
-            />
-            <RankCard
-              name="Mary Estudillo"
-              rank="5"
-              task="22"
-              hours="22:44"
-              score="-226"
-              totalScore="2266"
-              profilePic="user-5"
-            />
+            {users.map((user) => (
+              <RankCard {...user} />
+            ))}
           </tbody>
         </table>
       </div>
